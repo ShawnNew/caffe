6dfb16134e056e960b0823bc28ec505fbc26180e
ShawnNew <ncxinhanzhong@gmail.com>
2019-01-18 22:06:35 +0000

    add alpha loss c++ version, need to test.

---
+++ include/caffe/layers/alpha_prediction_loss_layer.hpp
@@ -0,0 +1,60 @@
+#ifndef CAFFE_ALPHA_PREDICTION_LOSS_LAYER_HPP_
+#define CAFFE_ALPHA_PREDICTION_LOSS_LAYER_HPP_
+
+#include <vector>
+
+#include "caffe/blob.hpp"
+#include "caffe/layer.hpp"
+#include "caffe/proto/caffe.pb.h"
+
+#include "caffe/layers/loss_layer.hpp"
+
+namespace caffe {
+
+/**
+ * 
+ * Compute alpha prediction loss for alpha matting problem. 
+ * Basically it is kind of euclidean loss; however, it is devided by the number of pixels.
+ *  
+ */
+
+template <typename Dtype>
+class AlphaPredictionLossLayer : public LossLayer<Dtype> {
+  public:
+    explicit AlphaPredictionLossLayer(const LayerParameter& param)
+        : LossLayer<Dtype>(param), diff_() {}
+    
+    virtual inline const char* type() const { return "AlphaPredictionLoss"; }
+
+    virtual void LayerSetUp(
+      const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top);
+
+    virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+
+    /**
+     * Backpropagate the gradient to the first bottom blob (prediction).
+     * Because only the first bottom blob is used to update the weights when training.
+     * So there is no need to override the AllowForceBackward function.
+     */
+    double alpha_loss(Dtype pred); // compute the loss and give value to diff_
+
+  protected:
+    virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+        const vector<Blob<Dtype>*>& top);
+    // virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+    //     const vector<Blob<Dtype>*>& top);
+    virtual void Backward_cpu(const vector<Blob<Dtype>*>& bottom,
+        const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& top);
+    // virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+    //     const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    
+    Blob<Dtype> diff_;
+    double epsilon = 1e-6;
+    vector<int> shape_img_;
+    vector<double> mask_;
+    int num_pixels;
+};
+}  // namespace caffe
+
+#endif
---
+++ src/caffe/layers/alpha_prediction_loss_layer.cpp
@@ -0,0 +1,77 @@
+#include <vector>
+
+#include "caffe/layers/alpha_prediction_loss_layer.hpp"
+#include "caffe/util/math_functions.hpp"
+
+namespace caffe {
+
+template <typename Dtype>
+void AlphaPredictionLossLayer<Dtype>::LayerSetUp(
+    const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) {
+    shape_img_.assign(
+        bottom[0]->shape().begin()+2,
+        bottom[0]->shape().end()-1 );  // get shape of the image: width*height
+}
+
+template <typename Dtype>
+void AlphaPredictionLossLayer<Dtype>::Reshape(
+    const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) {
+    LossLayer<Dtype>::Reshape(bottom, top);   // reshaped top shape here.
+    CHECK_EQ(bottom[0]->count(1), bottom[1]->count(1))
+      << "Inputs must have the same dimension.";
+    diff_.ReshapeLike(*bottom[0]);
+    // get mask and reshape here. 
+    // mask is stored as class member mask_
+    for (int i = 0; i < bottom[1]->count(); ++i) {  // over batch
+        for (int j = 0; j < shape_img_[0]; ++j) {   // over width
+            for (int k = 0; k < shape_img_[1]; ++k) { // over height
+                if (bottom[1]->data_at(i, 0, j, k) == 0. || \
+                    bottom[1]->data_at(i, 0, j, k) == 1.) {
+                        mask_.push_back(Dtype(0));
+                } else {
+                    num_pixels++;
+                    mask_.push_back(double(bottom[1]->data_at(i, 0, j, k)));
+                }
+                gt_.push_back(bottom[1]->data_at(i, 1, j, k));
+            }
+        }
+    }
+    
+}
+
+template <typename Dtype>
+void AlphaPredictionLossLayer<Dtype>::Forward_cpu(
+    const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) {
+    int count = bottom[0]->count;
+    Dtype* mul_;
+    caffe_sub(
+        count,
+        bottom[0]->cpu_data(),
+        gt_.data(),
+        diff_.mutable_cpu_data()
+    );
+    caffe_mul(count, diff_.cpu_data(), mask_.data(), mul_);  // multiply bu mask
+    Dtype dot = caffe_cpu_dot(count, mul_, mul_);            // square and sum
+    Dtype loss = dot / num_pixels / Dtype(2);
+    top[0]->mutable_cpu_data()[0] = loss;
+}
+
+template <typename Dtype>
+void AlphaPredictionLossLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
+    const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
+    for (int i = 0; i < 2; ++i) {
+        if (propagate_down[i]) {
+            const Dtype sign = (i == 0) ? 1 : -1;
+            // const Dtype alpha = sign * top[0]->cpu_diff()[0] / bottom[0]->num();
+            caffe_cpu_axpby(   // alpha*X + beta*Y
+                bottom[i]->count(),              // count
+                sign,                              // alpha
+                diff_.cpu_data(),                   // a
+                Dtype(0),                           // beta
+                bottom[i]->mutable_cpu_diff());  // b
+            // bottom[i]->mutable_cpu_diff() = sign * diff_.cpu_data();
+        }
+    }
+}
+
+}
